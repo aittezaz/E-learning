cd770bd261bddfa8a341e209bd39fcc1ebbe4ea8
Aittezaz Ahmad <aittezazahmad@gmail.com>
2025-07-08 13:54:13 +0500

    Initial commit

---
+++ html practice/jsScript.js
@@ -0,0 +1,63 @@
+// // Example of if Else
+// let marks = +prompt("enter your marks");
+// if( marks >90)
+// {
+// alert("you are a genious student ") }
+//  if(marks>80){
+//     alert(" you are a  good student");
+// }
+// if (marks>10) {
+//     alert("you are average student");
+// }
+
+
+// Example of switch
+//  let saufik = prompt("enter a name")
+//  switch(saufik) {
+// case "Ouneeb" :
+// console.log("he is a senior dev");
+// break;
+// case "Malaika" :
+// console.log("she is on junior level");
+// break;
+// case "Aittezaz" :
+// console.log("He is an Intern");
+// break;
+// default :
+// console.log("sorry dont know");
+// }
+  
+
+// Example of this in Object , a simple calculater
+
+
+// let calculater = {
+//     read(){
+// this.a = +prompt("enter a number" , 0)
+// this.b = +prompt("enter a  another number" , 0)
+
+//     } ,
+//     Sum(){
+//         return this.a + this.b ;
+//     } ,
+//     mul(){
+//         return this.a * this.b ;
+//     } ,
+//     Sub(){
+//         return this.a - this.b ;
+//     } ,
+//     div(){
+//         return this.a % this.b ;
+//     } 
+// } ;
+//     calculater.read();
+//     alert(calculater.Sum()) ;
+//     alert(calculater.Sub()) ;
+//     alert(calculater.mul()) ;
+//     alert(calculater.div()) ;
+    
+
+
+
+
+
